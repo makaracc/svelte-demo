7722ef20f135cb4872828a12a86d82ffcba4ac38
makaracc <makara.chhaing@gmail.com>
2022-02-17 23:55:45 +1100

    delete style

--- src/App.svelte
+++ src/App.svelte
@@ -12,21 +12,7 @@
 </main>
 
 <style>
-  :root {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-  }
 
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
 
  
 </style>
