4522e82ae30c47aaa20947d3f0b7ddff5787e921
makaracc <makara.chhaing@gmail.com>
2022-02-17 23:53:49 +1100

    button

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import logo from './assets/svelte.png'
   import Counter from './lib/Counter.svelte'
+
+  let show = false;
+  const setShow = () => show = !show;
 </script>
 
 <main>
-  <button>
-  <img src="https://svelte.dev/tutorial/image.gif">
+  <button on:click={setShow}> </button>
 </main>
 
 <style>
