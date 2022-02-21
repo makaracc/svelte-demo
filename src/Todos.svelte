138c77b3840f3abbc0392313df5130fbf270dcf3
makaracc <makara.chhaing@gmail.com>
2022-02-22 00:48:29 +1100

    add todos

---
+++ src/Todos.svelte
@@ -0,0 +1,16 @@
+<script>
+  import TodoItem from "./TodoItem.svelte";
+  import { db } from "./firebase";
+  import {collectionData} from 'rxfire/firestore';
+import {startWith} from 'rxjs/operators';
+
+export let uid;
+export let text = "some thing";
+const query = db.collection("todos").where("uid", "==", uid);
+
+
+const querySnapshot = await getDocs(collection(db, "users"));
+querySnapshot.forEach((doc) => {
+  console.log(`${doc.id} => ${doc.data()}`);
+});
+</script>
