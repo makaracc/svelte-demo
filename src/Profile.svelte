4ce44c0a1112e02516dd266e2d251f20c928f8ff
makaracc <makara.chhaing@gmail.com>
2022-02-22 00:47:50 +1100

    add profile

---
+++ src/Profile.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let name;
+  export let profile;
+  export let uid;
+</script>
+
+<main>
+  <h3>Hi {name}</h3>
+  <img src={profile} alt={`${name} profile`} width="100">
+  <h3>Your UID {uid}</h3>
+</main>
