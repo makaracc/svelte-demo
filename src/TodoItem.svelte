c686d3dd69fdcede54b713d3cb5f5a8cbcf6837b
makaracc <makara.chhaing@gmail.com>
2022-02-22 00:47:43 +1100

    add todoitem

---
+++ src/TodoItem.svelte
@@ -0,0 +1,32 @@
+<script>
+  import {fly, fade} from 'svelte/transition';
+  import {createEventDispatcher} from 'svelte';
+  const dispatch = createEventDispatcher();
+
+  function remove () {
+   dispatch('remove', {id});
+  }
+
+  function toggleStatus () {
+    let status = !complete;
+   dispatch('toggle', {id, newStatus});
+  }
+
+  export let id;
+  export let complete;
+  export let text;
+  export let newStatus;
+</script>
+
+<li>
+  <p>{text}</p>
+  {#if complete}
+    <button on:click={toggleStatus}>
+      y
+    </button>
+  {:else}
+    <button on:click={toggleStatus}>
+      x
+    </button>
+  {/if}
+</li>
